ec82f7231e35686af07d811a89d89afd41bfe10c
Adit310304 <muhammadadityafirman57@gmail.com>
2025-09-15 21:46:13 +0700

    Answer number 43

---
+++ 0043_Multiply_Strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        int_num1 = int(num1)
+        int_num2 = int(num2)
+
+        res = int_num1 * int_num2
+
+        return str(res)
