fe4ecdccfb9227f31609d5549821d36a8329d68f
Adit310304 <muhammadadityafirman57@gmail.com>
2025-09-15 22:41:57 +0700

    Answer number 217

---
+++ 0217_Contains_Duplicate.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums_set = set(nums)
+
+        if len(nums_set) == len(nums):
+            return False
+        
+        return True
