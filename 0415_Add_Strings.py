cafc95f419700c0f1552ded226a34dd7537cfba4
Adit310304 <muhammadadityafirman57@gmail.com>
2025-09-15 22:49:52 +0700

    Answer number 415

---
+++ 0415_Add_Strings.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        int_num1 = int(num1)
+        int_num2 = int(num2)
+
+        result = int_num1 + int_num2
+
+        return str(result)
