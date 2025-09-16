af7cc61efd21ead07b2fffde41507fc9c4ccf8a2
Adit310304 <muhammadadityafirman57@gmail.com>
2025-09-16 08:02:54 +0700

    Answer number 1920

---
+++ 1920_Build_Array_From_Permutation.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def buildArray(self, nums):
+        result = []
+
+        for i in range(len(nums)):
+            result.append(nums[nums[i]])
+
+        return result
