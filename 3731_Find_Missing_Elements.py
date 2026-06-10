92644eedbac75c7d7aeb0898abf4a7a7a9c484a3
Adit310304 <muhammadadityafirman57@gmail.com>
2026-06-11 02:20:44 +0700

    Answer number 3731

---
+++ 3731_Find_Missing_Elements.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        res = []
+
+        for i in range(min(nums), max(nums) + 1):
+            if i not in nums:
+                res.append(i)
+        
+        return res
